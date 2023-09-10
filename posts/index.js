6dc05b1e78a7ac5b94352aa5f5eba5af9a0a25f2
John Q <ikram1981@gmail.com>
2023-09-09 22:02:02 -0500

    posts server is running

--- posts/index.js
+++ posts/index.js
@@ -1,17 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
-const app =express();
-const posts ={};
 
+const app = express();
+app.use(bodyParser.json());
 
- app.get('/posts',(req, res)=>{
-    res.send(posts);
- });
+const posts = {};
 
- app.post('/posts',(req, res)=>{
-    const id = 
- });
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
 
- app.listen(4000,()=>{
-    console.log('Listening on 4000');
- });
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title
+  };
+
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log('Listening on 4000');
+});
