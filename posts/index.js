1ff75081df1f7e61e99cedb43726bf1ca427ed98
John Q <ikram1981@gmail.com>
2023-09-09 21:30:58 -0500

    added

---
+++ posts/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+const app =express();
+const posts ={};
+
+
+ app.get('/posts',(req, res)=>{
+    res.send(posts);
+ });
+
+ app.post('/posts',(req, res)=>{
+    const id = 
+ });
+
+ app.listen(4000,()=>{
+    console.log('Listening on 4000');
+ });
