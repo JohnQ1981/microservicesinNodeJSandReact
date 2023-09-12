d248bb249295ab4955df112b2d1384fe49f3d5e5
JohnQ1981 <ikram1981@gmail.com>
2023-09-11 21:21:47 -0500

    fixed

--- query/index.js
+++ query/index.js
@@ -5,33 +5,34 @@ const cors = require('cors');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
 const posts = {};
 
-
-app.get('/posts',(req, res)=>{
-    res.send(posts);
-
+app.get('/posts', (req, res) => {
+  res.send(posts);
 });
 
-app.post('/events',(req, res)=>{
-    const { type , data } = req.body;
-    if(type === 'postCreated'){
-        const { id , title } = data;
-        posts[id] = {id , title, comments: []};
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
 
-    }
-    if(type ==='CommentCreated'){
+  if (type === 'PostCreated') {
+    const { id, title } = data;
 
-        const {id, content, postId} = data;
-        const post = post[postId];
-        post.comments.push({id, content});
+    posts[id] = { id, title, comments: [] };
+  }
 
-    }
-    console.log(posts);
-        res.send({});
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
 
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  console.log(posts);
+
+  res.send({});
 });
 
-app.listen(4002,()=>{
-    console.log('Listening on 4002');
+app.listen(4002, () => {
+  console.log('Listening on 4002');
 });
