9f99f063f0e61e457559771f0af16e37d0114844
JohnQ1981 <ikram1981@gmail.com>
2023-09-11 21:43:59 -0500

    working again

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -3,11 +3,12 @@ import axios from "axios";
 import CommentCreate from "./CommentCreate";
 import CommentList from "./CommentList";
 
-export default () => {
+const PostList = () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:4000/posts");
+    const res = await axios.get("http://localhost:4002/posts");
+    
     setPosts(res.data);
   };
 
@@ -15,7 +16,6 @@ export default () => {
     fetchPosts();
   }, []);
 
-  //console.log(posts);
   const renderedPosts = Object.values(posts).map((post) => {
     return (
       <div
@@ -25,17 +25,18 @@ export default () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
+          <CommentList comments= {post.comments} />
           <CommentCreate postId={post.id} />
-          
         </div>
       </div>
     );
   });
 
   return (
-    <div className="d-flex flex-row flex-wrap justify-content-between ">
+    <div className="d-flex flex-row flex-wrap justify-content-between">
       {renderedPosts}
     </div>
   );
 };
+
+export default PostList;
